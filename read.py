a4196519d39c589a571d7fba3f474509de91ae5a
Whitelisted <77711834+Whitelisted1@users.noreply.github.com>
2022-03-20 09:33:22 -0600

    Update read.py

--- read.py
+++ read.py
@@ -8,17 +8,26 @@ seconds_in_day = 60 * 60 * 24
 seconds_in_hour = 60 * 60
 seconds_in_minute = 60
 
+
+times = []
+for app in apps:
+    if app != "":
+        time = float(app.split(":")[1])
+        times.append(time)
+totaltime = sum(times)
+
 if apps != [""]:
     print("[App Name] - [days]:[hours]:[minutes]:[seconds]")
-    for app in apps:
+    for i, app in enumerate(apps):
         if app != "":
             splitapp = app.split(":")
             seconds = int(round(float(splitapp[1])))
+            totalseconds = seconds
             days = seconds // seconds_in_day
             hours = (seconds - (days * seconds_in_day)) // seconds_in_hour
             minutes = (seconds - (days * seconds_in_day) - (hours * seconds_in_hour)) // seconds_in_minute
             seconds = (seconds - (days * seconds_in_day) - (hours * seconds_in_hour) - (minutes * seconds_in_minute))
-            print(f"{splitapp[0]} - {days}:{hours}:{minutes}:{seconds}")
+            print(f"{splitapp[0]} - {days}:{hours}:{minutes}:{seconds} (≈{round(totalseconds/totaltime*100, 2)}%)")
 else:
     print("No time information is available")
 
